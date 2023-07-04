ac2e22e3910423bbfdd248b510b4e12e151fd27f
barzeli <evya7777777@gmail.com>
2023-07-05 00:20:46 +0300

    added ts

---
+++ src-ts/Node.ts
@@ -0,0 +1,11 @@
+class Node {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+
+    this.checked = false;
+    this.minMovesToPoint = 10000;
+    this.edges = [];
+  }
+}
